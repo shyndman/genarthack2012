b1760e790b5560d850c625fd36ff42ed28b6647e
Darrick Wiebe <dw@xnlogic.com>
2013-02-16 13:12:45 -0500

    back to localhost

--- server/public/client.js
+++ server/public/client.js
@@ -1,10 +1,13 @@
 window.wsurl = "ws://ec2-184-73-104-159.compute-1.amazonaws.com";
+window.wsurl = "ws://localhost:8080";
 window.wsCtor = window['MozWebSocket'] ? MozWebSocket : WebSocket;
 
 function simulate() {
   return new wsCtor(wsurl, 'onephone');
 }
 
+// Message JSON object:
+// tiltLR, tiltFB, dir, touches
 function onePhoneEvents(on_message, on_close) {
   this.socket = new wsCtor(wsurl, 'onephoneConsumer');
   this.socket.onmessage = function(message) {
