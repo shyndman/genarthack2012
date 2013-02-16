f122bc13e59f28c4f8a7eb7570f84abe16ea2bdc
Darrick Wiebe <dw@xnlogic.com>
2013-02-16 12:28:54 -0500

    Correct urls

--- server/public/client.js
+++ server/public/client.js
@@ -1,11 +1,11 @@
-window.wsurl = "ws://localhost:8080";
+window.wsurl = "ws://ec2-184-73-104-159.compute-1.amazonaws.com";
 window.wsCtor = window['MozWebSocket'] ? MozWebSocket : WebSocket;
 
 function simulate() {
   return new wsCtor(wsurl, 'onephone');
 }
 
-function onePhoneEvents(on_message, on_close, url) {
+function onePhoneEvents(on_message, on_close) {
   this.socket = new wsCtor(wsurl, 'onephoneConsumer');
   this.socket.onmessage = function(message) {
     var data = JSON.parse(message.data);
